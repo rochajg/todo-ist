371515eb5e9dd74e8acccac73899fea78621f41a
Jordan Rocha <70rd23@gmail.com>
2018-09-16 03:12:00 -0300

    Estruturando o sistema de pastas do projeto

---
+++ src/template/mainHeader.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <nav>
+    <div class="nav-wrapper">
+      <a href="/" class="brand-logo">ToDo-ist</a>
+      <a href data-target="mobile-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><router-link to="list">Lista</router-link></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-menu">
+    <li><router-link to="list">Lista</router-link></li>
+  </ul>
+</div>
+</template>
+
+<script>
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  })
+
+export default {
+  name: 'mainHeader',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
