83fdc84da7061f6f7f9d8e9b055ba2ed4735f823
Jordan Rocha <70rd23@gmail.com>
2018-09-16 03:45:07 -0300

    Configuração da página inicial da applicação

---
+++ src/pages/home/home.vue
@@ -0,0 +1,20 @@
+<template>
+	<main class="row">
+		<div class="col s12">
+			<h2>Bem Vindo ao ToDo-ist</h2>
+			<blockquote>
+				Sua aplicação de gerenciamento de atividades e a fazeres!
+			</blockquote>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	name: 'home',
+	data () {
+			return {
+			}
+	}   
+}
+</script>
--- src/template/mainHeader.vue
+++ src/template/mainHeader.vue
@@ -1,19 +1,21 @@
 <template>
-<div>
-    <nav>
-    <div class="nav-wrapper">
-      <a href="/" class="brand-logo">ToDo-ist</a>
-      <a href data-target="mobile-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-        <li><router-link to="list">Lista</router-link></li>
-      </ul>
-    </div>
-  </nav>
+  <div>
+      <nav>
+      <div class="nav-wrapper">
+        <a href="/" class="brand-logo">ToDo-ist</a>
+        <a href data-target="mobile-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><router-link to="/">Início</router-link></li>
+          <li><router-link to="list">Lista</router-link></li>
+        </ul>
+      </div>
+    </nav>
 
-  <ul class="sidenav" id="mobile-menu">
-    <li><router-link to="list">Lista</router-link></li>
-  </ul>
-</div>
+    <ul class="sidenav" id="mobile-menu">
+      <li><router-link to="/">Início</router-link></li>
+      <li><router-link to="list">Lista</router-link></li>
+    </ul>
+  </div>
 </template>
 
 <script>
