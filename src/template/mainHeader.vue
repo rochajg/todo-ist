791a0da7f0c7810fa135be169b8e1126a107b8ad
Jordan Rocha <70rd23@gmail.com>
2018-09-16 18:51:43 -0300

    Funcionalidade para fechar navegação automaticamente

--- src/template/mainHeader.vue
+++ src/template/mainHeader.vue
@@ -6,17 +6,17 @@
           <a href="/" class="brand-logo">ToDo-ist</a>
           <a href data-target="mobile-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
           <ul class="right hide-on-med-and-down">
-            <li><router-link to="/">Início</router-link></li>
-            <li><router-link to="list">Lista</router-link></li>
-            <li><router-link to="stats">Estatísticas</router-link></li>
+            <li><router-link class="sidenav-close" to="/">Início</router-link></li>
+            <li><router-link class="sidenav-close" to="list">Lista</router-link></li>
+            <li><router-link class="sidenav-close" to="stats">Estatísticas</router-link></li>
           </ul>
         </div>
       </nav>
     </div>  
     <ul class="sidenav" id="mobile-menu">
-      <li><router-link to="/">Início</router-link></li>
-      <li><router-link to="list">Lista</router-link></li>
-      <li><router-link to="stats">Estatísticas</router-link></li>
+      <li><router-link class="sidenav-close" to="/">Início</router-link></li>
+      <li><router-link class="sidenav-close" to="list">Lista</router-link></li>
+      <li><router-link class="sidenav-close" to="stats">Estatísticas</router-link></li>
     </ul>
   </main>
 </template>
